d7c58328728b96ce8e9b83dd407622d1e4e922b6
Amanjeet Kumar <techamanjeet1@gmail.com>
2021-05-11 04:03:58 +0530

    lession 1 2nd q. solve

---
+++ Introduction to Programming with MATLAB/Lesson 1 Wrap-up.m
@@ -0,0 +1,5 @@
+times=9.58;
+distancem=100;
+distance=distancem/1000;
+time=times/3600;
+hundred=distance/time
---
+++ Introduction to Programming with MATLAB/lesson_1_wrap_up.m
@@ -0,0 +1,9 @@
+times=9.58;
+distancem=100;
+distance=distancem/1000;
+time=times/3600;
+hundred=distance/time
+
+distance=42.195;
+time=2+(1/60)+(39/3600);
+marathon=distance/time
