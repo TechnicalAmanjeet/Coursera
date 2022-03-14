d6237aaf7d67e0029bafabab7dc12474d5832dbd
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-03-14 23:09:39 +0530

    Starting with java course specialization provided by duke university. this is the weak 1 of first course.

---
+++ Specialization/Java Programming and Software Engineering Fundamentals Specialization/1.Programming Foundations with JavaScript, HTML and CSS/Weak1/src/com/Amanjeet/Main.java
@@ -0,0 +1,9 @@
+package com.Amanjeet;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Starting with java course specialization provided by duke university. this is the weak 1 of first course.");
+    }
+}
