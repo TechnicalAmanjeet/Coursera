4c55730b69f42bbd42a92d4392dddd494ee0f874
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-05-13 21:08:39 +0530

    done with weak 1 of using python to access web data course

---
+++ Specialization/Python for Everybody Specialization/3.Using Python to Access Web Data/Weak01/1.Peer Review Installing and Running Python Screen Shots/main.py
@@ -0,0 +1,5 @@
+print("""
+Hii Guy's This is Technical Amanjeet here. 
+on ( 13-05-2022 ) i'm going to start a course called 'Using python to access web data'
+wish me luck.
+Thank you guy's in advance for reviewing my assignment..""")
